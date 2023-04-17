c8d2f215796ceb9930f2dec535e9b1fe00529134
SafarovFarid97 <safarovfarid97@mail.ru>
2023-04-17 09:48:06 +0300

    HibernateUtil

---
+++ src/main/java/jm/task/core/jdbc/util/HibernateUtil.java
@@ -0,0 +1,59 @@
+package jm.task.core.jdbc.util;
+
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class HibernateUtil implements AutoCloseable{
+    public static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+
+            try {
+                Configuration configuration = new Configuration();
+
+                // Hibernate settings equivalent to hibernate.cfg.xml's properties
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/user?useUnicode=yes&characterEncoding=UTF-8");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "1q2w3e4r5t6Y7");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "");
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
+    }
+
+    @Override
+    public void close() throws Exception {
+        sessionFactory.close();
+    }
+}
